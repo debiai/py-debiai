b7598ec649807a16c76d60762f314475db3353a0
tom.mansion <tomansion@yahoo.fr>
2022-03-29 17:26:17 +0200

    0.15.2 - tests only on pr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="debiai",
-    version="0.15.1",
+    version="0.15.2",
     author="IRT-SystemX",
     author_email="debiai@irt-systemx.fr",
     description="DebiAI python module",
