7bf220067937a869e9de293729e2618930c28828
tom.mansion <tomansion@yahoo.fr>
2022-03-29 17:46:21 +0200

    0.15.4 - fixed publish action

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="debiai",
-    version="0.15.3",
+    version="0.15.4",
     author="IRT-SystemX",
     author_email="debiai@irt-systemx.fr",
     description="DebiAI python module",
